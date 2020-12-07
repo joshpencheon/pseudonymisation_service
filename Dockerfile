4fcb4f06af73508ae048df07c89e6634efcb64cf
Josh Pencheon <josh.pencheon@googlemail.com>
2020-12-07 20:42:16 +0000

    # Add basic Dockerisation
    - Add Dockerfile
    - Add Compose configuration for PG too
    - Add in some necessary seed data for bootstrapping a DB
    - Update sample DB configuration to allow values from the environment
    - Ensure secret_key_base is present in the included secrets bundle

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM ruby:2.7.0
+
+RUN apt-get update -qq && apt-get install -y postgresql-client cmake
+
+WORKDIR /app
+COPY Gemfile Gemfile
+COPY Gemfile.lock Gemfile.lock
+
+RUN bundle install
+COPY . .
+
+# Some hacky bootstrapping:
+RUN echo *** Using application test configuration for image build ***
+RUN rm config/puma.rb
+RUN mv config/database.yml.sample config/database.yml
+RUN cp config/userlist_test.yml config/userlist.yml
+RUN cp config/credentials/test.key config/credentials/production.key
+RUN cp config/credentials/test.yml.enc config/credentials/production.yml.enc
+
+EXPOSE 3000
+
+CMD ["rails", "server", "-b", "0.0.0.0"]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+## Users ##
+
+test_user = User.create!(username: 'test_user')
+
+## PseudonymistionKeys ##
+
+primary_one = PseudonymisationKey.create!(name: 'Primary Key One')
+primary_two = PseudonymisationKey.create!(name: 'Primary Key Two')
+
+repseudo_one = PseudonymisationKey.create!(name: 'RePseudo Key One', parent_key: primary_one)
+repseudo_two = PseudonymisationKey.create!(name: 'RePseudo Key Two', parent_key: primary_two)
+_repseudo_three = PseudonymisationKey.create!(name: 'RePseudo Key Three', parent_key: repseudo_two)
+
+_compound_one = PseudonymisationKey.create!(
+  key_type: 'compound',
+  name: 'Compound Key One',
+  start_key: primary_one,
+  end_key: repseudo_one
+)
+
+compound_two = PseudonymisationKey.create!(
+  key_type: 'compound',
+  name: 'Direct access to RePseudo One',
+  start_key: repseudo_one,
+  end_key: repseudo_one
+)
+
+## KeyGrants ##
+
+_grant_one = KeyGrant.create!(user: test_user, pseudonymisation_key: primary_one)
+_grant_two = KeyGrant.create!(user: test_user, pseudonymisation_key: compound_two)
